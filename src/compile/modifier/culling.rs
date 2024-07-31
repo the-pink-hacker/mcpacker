9b7614505ecae5187a52660adac0ada99af69990
Pink Garrett <pink@thepinkhacker.com>
2024-07-31 13:37:55 -0400

    Fix empty cubes not being culled

--- src/compile/modifier/culling.rs
+++ src/compile/modifier/culling.rs
@@ -54,8 +54,6 @@ impl ModelElement {
         let within_y = from_y >= other_from_y && to_y <= other_to_y;
         let within_z = from_z >= other_from_z && to_z <= other_to_z;
 
-        let mut empty_faces = 0u8;
-
         if self.faces.north.is_some()
             && other.faces.south.is_some()
             && within_x
@@ -63,7 +61,6 @@ impl ModelElement {
             && from_z == other_to_z
         {
             self.faces.north.take();
-            empty_faces += 1;
         }
 
         if self.faces.east.is_some()
@@ -73,7 +70,6 @@ impl ModelElement {
             && to_x == other_from_x
         {
             self.faces.east.take();
-            empty_faces += 1;
         }
 
         if self.faces.south.is_some()
@@ -83,7 +79,6 @@ impl ModelElement {
             && to_z == other_from_z
         {
             self.faces.south.take();
-            empty_faces += 1;
         }
 
         if self.faces.west.is_some()
@@ -93,7 +88,6 @@ impl ModelElement {
             && from_x == other_to_x
         {
             self.faces.west.take();
-            empty_faces += 1;
         }
 
         if self.faces.up.is_some()
@@ -103,7 +97,6 @@ impl ModelElement {
             && to_y == other_from_y
         {
             self.faces.up.take();
-            empty_faces += 1;
         }
 
         if self.faces.down.is_some()
@@ -113,10 +106,9 @@ impl ModelElement {
             && from_y == other_to_y
         {
             self.faces.down.take();
-            empty_faces += 1;
         }
 
         // All faces have been culled.
-        empty_faces == 6
+        (&self.faces).into_iter().all(|face| face.is_none())
     }
 }
